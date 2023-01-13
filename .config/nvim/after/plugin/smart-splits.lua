9b8b98d4b5a226729e1047754b73186ee3397f49
Uriel Coria <urielcoria@lyft.com>
2023-01-13 11:21:30 -0600

    fix tmux conf and removes split stuff

--- .config/nvim/after/plugin/smart-splits.lua
+++ .config/nvim/after/plugin/smart-splits.lua
@@ -60,8 +60,6 @@ ssplits.setup({
 -- these keymaps will also accept a range,
 -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
 
-require('smart-splits').start_resize_mode()
-vim.keymap.set('n', '', ssplits.start_resize_mode)
 vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
 vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
 vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
