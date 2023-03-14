34d85078ce0d32bdeefcf1ff64aca66be3660148
Uriel Coria <urielcoria@lyft.com>
2023-03-14 12:55:15 -0600

    lsp configs kotlin and swif

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,18 +1,33 @@
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
-require'lspconfig'.sourcekit.setup{}
 
+-- Add LSP Config for Lyft langs
+require'lspconfig'.sourcekit.setup{}
+require'lspconfig'.kotlin_language_server.setup{}
 -- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
