891007b079e664bcee138c58bb6c9799fceefa99
Uriel Coria <urielcoria@lyft.com>
2023-11-25 20:03:52 -0600

    awesome config and treesitter parsers

---
+++ .config/nvim/lua/plugins/treesitter-parsers.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "dockerfile",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "vim",
+      "yaml",
+    },
+  },
+}
