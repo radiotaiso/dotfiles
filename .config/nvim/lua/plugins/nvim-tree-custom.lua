a4725957e85c49663585bbcd88deebc7216ce450
Uriel Coria <urielcoria@lyft.com>
2023-11-30 10:53:25 -0600

    neotree options

--- .config/nvim/lua/plugins/nvim-tree-custom.lua
+++ .config/nvim/lua/plugins/nvim-tree-custom.lua
@@ -1,19 +1,26 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-       filesystem = {
-       filtered_items = {
-       visible = true,
-       show_hidden_count = true,
-       hide_dotfiles = false,
-       hide_gitignored = true,
-       hide_by_name = {
-         -- '.git',
-         -- '.DS_Store',
-         -- 'thumbs.db',
-       },
-      never_show = {},
-            },
-        }
-    }
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+    group_empty_dirs = true,
+    follow_current_file = {
+      enable = true,
+      hijack_cursor = true,
+      update_cwd = true,
+    },
+  },
 }
+
