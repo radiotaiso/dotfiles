5698977d3a6eb2b5564ea309e4929df503d9765e
Uriel Coria <urielcoria@lyft.com>
2024-02-27 10:10:43 -0600

    nvimtree pimp

--- .config/nvim/lua/plugins/nvim-tree-custom.lua
+++ .config/nvim/lua/plugins/nvim-tree-custom.lua
@@ -16,6 +16,24 @@ return {
         never_show = {},
       },
     },
+    git_status = {
+      symbols = {
+        -- Change type
+        added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = "✖", -- this can only be used in the git_status source
+        renamed = "󰁕", -- this can only be used in the git_status source
+        -- Status type
+        untracked = "",
+        ignored = "",
+        unstaged = "󰄱",
+        staged = "",
+        conflict = "",
+      },
+    },
+    window = {
+      width = 30,
+    },
     group_empty_dirs = true,
     follow_current_file = {
       enabled = true,
@@ -23,4 +41,3 @@ return {
     },
   },
 }
-
